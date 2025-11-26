ff0a8f46b0129202a2c3cfbfca9340dbdffd2170
pSus365 <suotr12@gmail.com>
2025-11-26 19:59:52 +0100

    HttpDelete CRUD operation added and tested

--- WebDiaryAPI/Controllers/DiaryEntriesController.cs
+++ WebDiaryAPI/Controllers/DiaryEntriesController.cs
@@ -29,7 +29,7 @@ namespace WebDiaryAPI.Controllers
             // Database 
             var diaryEntry = await _context.DiaryEntries.FindAsync(id);
 
-            if(diaryEntry == null)
+            if (diaryEntry == null)
             {
                 return NotFound();
             }
@@ -42,12 +42,62 @@ namespace WebDiaryAPI.Controllers
         {
             diaryEntry.Id = 0; // Ensure the ID is set to 0 for new entries (automaticaly sets the good id)
 
-            _context.DiaryEntries.Add(diaryEntry);  
+            _context.DiaryEntries.Add(diaryEntry);
             await _context.SaveChangesAsync();
 
             var resourceUrl = Url.Action(nameof(GetDiaryEntry), new { id = diaryEntry.Id });
 
             return Created(resourceUrl, diaryEntry);
+        }
+
+        // put something on e.g. /api/DiaryEntries/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDiaryEntry(int id, [FromBody] DiaryEntry diaryEntry)
+        {
+            if (id != diaryEntry.Id)
+            {
+                // returns 400 response when id do not match
+                return BadRequest();
+            }
+
+            // mark the entity as modified
+            _context.Entry(diaryEntry).State = EntityState.Modified; // tells EF to update this entity
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DiaryEntryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent(); // returns 204 response
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDiaryEntry(int id)
+        {
+           var diaryEntry = await _context.DiaryEntries.FindAsync(id);
+            if (diaryEntry == null)
+            {
+                return NotFound();
+            }
+            _context.DiaryEntries.Remove(diaryEntry);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool DiaryEntryExists(int id)
+        {
+            return _context.DiaryEntries.Any(e => e.Id == id);
         }
     }
 }
